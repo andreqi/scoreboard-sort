0df26e5b7e2c08a123c63470cf6e6b2833e70684
andreqi <drol.005@gmail.com>
2014-09-03 01:13:03 -0500

    acumulate in parser

--- assets/javascript/parser.js
+++ assets/javascript/parser.js
@@ -4,13 +4,15 @@ var parse = function(file) {
   lines.forEach(function(line) {
     var tokens = line.trim().split(/\s+/); 
     var name = tokens[0], points = tokens.slice(1);
+    var sum = 0;
     points.forEach(function(acum, index) {
       if (contest.length <= index) {
         contest.push([]);
       }
+      sum += parseFloat(acum);
       contest[index].push({
         handle: name, 
-        points: parseFloat(acum),
+        points: sum,
       });
     });  
   });
